b7c345d7e915c9238adad4cbc48b3aa87a80a83f
Mark Finger <markfinger@gmail.com>
2019-06-06 15:58:35 +1000

    PyPI fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 Changelog
 =========
 
+### 1.0.1 (06/06/2019)
+
+- PyPI fix.
+
 
 ### 1.0.0 (06/06/2019)
 
-- Initial release
+- Initial release.
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="wagtail-instance-selector",
     version=instance_selector.__version__,
     packages=["instance_selector"],
+    include_package_data=True,
     description="A widget for Wagtail's admin that allows you to create and select related items",
     long_description="Documentation at https://github.com/ixc/wagtail-instance-selector",
     author="The Interaction Consortium",
