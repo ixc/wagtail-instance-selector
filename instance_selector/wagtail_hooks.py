6c2e15d4d35e3dd1b64476edbefa472dc757ff20
Jorge Garcia <j.hrique@hotmail.com>
2020-03-13 12:52:25 -0300

    Running in Django 3.0

--- instance_selector/wagtail_hooks.py
+++ instance_selector/wagtail_hooks.py
@@ -1,5 +1,5 @@
 from django.utils.html import format_html
-from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.conf.urls.static import static
 from wagtail.core import hooks
 from instance_selector import urls
 
