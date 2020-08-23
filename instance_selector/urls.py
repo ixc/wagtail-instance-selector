0d0e3eba1a4d1ca835180401601e9fe6abbc30f0
Jérôme Lebleu <jeromelebleu@users.noreply.github.com>
2020-08-23 09:40:20 +0200

    Replace calls to django.conf.urls.url with django.urls.path or re_path

--- instance_selector/urls.py
+++ instance_selector/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import re_path
 from instance_selector.views import instance_selector_embed, instance_selector_lookup
 
 
 urlpatterns = [
-    url(
+    re_path(
         r"^instance-selector/embed/(?P<app_label>[\w-]+).(?P<model_name>[\w-]+)/$",
         instance_selector_embed,
         name="wagtail_instance_selector_embed",
     ),
-    url(
+    re_path(
         r"^instance-selector/lookup/(?P<app_label>[\w-]+).(?P<model_name>[\w-]+)/$",
         instance_selector_lookup,
         name="wagtail_instance_selector_lookup",
